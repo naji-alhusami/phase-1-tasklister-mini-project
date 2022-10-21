69289009f8fd671473c37f5c98825aed9906ec3c
naji-alhusami <naji.alhusami@bahcesehir.edu.tr>
2022-10-21 15:42:42 +0300

    Completed assignemnt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
+const cTF = document.getElementById('create-task-form')
+
+cTF.addEventListener("click", (event) => {
   // your code here
+  event.preventDefault();
 });
